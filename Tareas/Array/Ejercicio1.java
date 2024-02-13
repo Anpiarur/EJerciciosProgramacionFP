2f952e50e6445ce9475a309b77a4c5dcb180786e
anpiarur <angelaruizdev@gmail.com>
2024-02-13 14:06:28 +0100

    packageAdded

---
+++ Tareas/Array/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Tareas.Array;
+
+/**
+ * 1. Diseñar un array, que almacena los números del 1 al 10. Consideraciones:
+ * Utilizar un bucle for para cargar el array
+ * Se deben imprimir los datos del array en sentido inverso
+ */
+public class Ejercicio1 {
+public static void main(String[] args) {
+    
+}
+}
